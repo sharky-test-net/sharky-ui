750f9308c7bf210be93ca60d9b972b53d9e15a6c
Valera Rozuvan <valera.rozuvan@gmail.com>
2019-11-22 23:25:09 +0200

    Initial commit.

---
+++ build-push.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+IMAGE="sharky-ui"
+VERSION="00007"
+
+docker build -t ${IMAGE}:${VERSION} . && \
+docker tag ${IMAGE}:${VERSION} "gcr.io/sharky-259408/${IMAGE}:latest" && \
+docker tag ${IMAGE}:${VERSION} "gcr.io/sharky-259408/${IMAGE}:${VERSION}" && \
+docker push "gcr.io/sharky-259408/${IMAGE}:latest" && \
+docker push "gcr.io/sharky-259408/${IMAGE}:${VERSION}"
+
+exit 0
